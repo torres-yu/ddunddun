087b36fab7c836e5d5a311653b5a6e26f6f90ac3
torres(유용상) <torres.yu@kakaocorp.com>
2021-02-26 15:28:13 +0900

    Initial commit

---
+++ src/main/java/io/torres/ddunddun/DdunddunApplication.java
@@ -0,0 +1,13 @@
+package io.torres.ddunddun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DdunddunApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DdunddunApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/torres/ddunddun/DdunddunApplicationTests.java
@@ -0,0 +1,13 @@
+package io.torres.ddunddun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DdunddunApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
